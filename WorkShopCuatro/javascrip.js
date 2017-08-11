041dbf0ef5e914fe03623a22f20831b824c51098
Edwin <ejimenezu@est.utn.ac.cr>
2017-08-11 11:03:20 -0600

    Cambios

--- WorkShopCuatro/javascrip.js
+++ WorkShopCuatro/javascrip.js
@@ -1,24 +1,56 @@
+$(document).ready(function(){    
     
-function Guardando(){        
-       
-        
-        /*Captura de datos escrito en los inputs*/        
-        var nom = document.getElementById("nombre").value;
-        var apel = document.getElementById("apellido").value;
-        var tel = document.getElementById("telefono").value;
-        
-        
-        /*Guardando los datos en el LocalStorage*/
-        localStorage.setItem("nombre", nombre);
-        localStorage.setItem("apellido", apellido);
-        localStorage.setItem("telefono", telefono);
-        
-       
-        /*Limpiando los campos o inputs*/
-        document.getElementById("nombre").value = "";
-        document.getElementById("apellido").value = "";
-        document.getElementById("telefono").value = "";
-        document.getElementById("correo").value = "";
-        
+    $('#btnSave').click(function(){        
+        var user = {
+                name: $('#nombre').val(),
+                lastname: $('#apellido').val(),
+                phone:$('#telefono').val()
+               
+          };
+var users = JSON.parse(localStorage.getItem('users'));
+if(!users){
+users= [];
+
+}
+         users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
      
-    };   
+     });
+
+});
+
+
+$(document).ready(function(){    
+    
+    $('#btnSave').click(function(){        
+    var user = {
+                name: $('#nombre').val(),
+                lastname: $('#apellido').val(),
+                phone:$('#telefono').val()
+               
+          };
+var users = JSON.parse(sessionStorage.getItem('users'));
+if(!users){
+users= [];
+
+}
+         users.push(user);
+        sessionStorage.setItem('users', JSON.stringify(users));
+     
+     });
+
+});
+
+ $(document).ready(function(){    
+    $('#btnDelete').click(function(){                
+                                                   
+     
+     var users = JSON.parse(localStorage.getItem('users'));
+       
+       users.pop(); 
+           localStorage.setItem('users', JSON.stringify(users));
+
+
+    });   
+});
+ 
